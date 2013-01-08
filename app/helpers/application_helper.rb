d7e070259d0e3538b66976b9a533d37472610b5f
Gary Geisler <ggeisler@gmail.com>
2013-01-08 10:02:04 -0800

    Clean up display of Item Details metadata - BASSI-9

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
     mvf.join(', ')
   end
   
+  def show_formatted_list(mvf)
+    content_tag(:ul, :class => "item-mvf-list") do
+      mvf.collect do |val|
+        content_tag(:li, val)
+      end.join.html_safe
+    end
+  end
+
+  def list_is_empty?(arry)
+    if arry.all? { |element| element.blank? }
+      return true
+    end
+  end
+
 end
