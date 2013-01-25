9b024bbda6ed3039bfc6c0ccc6662b4082e613a5
Peter Mangiafico <peter@mangiafico.org>
2013-01-25 11:40:27 -0800

    fix some non-JS issues; use purl embed plugin on item detail page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BassiVeratti::Application.initialize!
+
+BassiVeratti::Application.config.purl_plugin_server = "prod"
+BassiVeratti::Application.config.purl_plugin_location = "http://purl.stanford.edu/javascripts/purl_embed_jquery_plugin.js"
+BassiVeratti::Application.config.purl = "http://purl.stanford.edu"
