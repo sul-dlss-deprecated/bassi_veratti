97bdb214e43f4c9a2ff2546aedbd150bc1f7cae0
Jessie Keck <jessie.keck@gmail.com>
2013-02-06 15:42:17 -0800

    Try deploy:migrate instead of db:migrate.

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,6 +89,6 @@ namespace :deploy do
   end
 end
 
-after "deploy:update_code", "db:migrate"
+after "deploy:update_code", "deploy:migrate"
 after "deploy", "app:expire_caches"
 after "deploy", "app:add_date_to_version"
