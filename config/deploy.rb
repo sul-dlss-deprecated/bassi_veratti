39c583dc716c20ba131b26044a3b279cff597687
Jessie Keck <jessie.keck@gmail.com>
2013-02-06 15:54:02 -0800

    Try doing database migration after finalize update.

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,6 +89,6 @@ namespace :deploy do
   end
 end
 
-after "deploy:update_code", "deploy:migrate"
+after "deploy:finalize_update", "db:migrate"
 after "deploy", "app:expire_caches"
 after "deploy", "app:add_date_to_version"
