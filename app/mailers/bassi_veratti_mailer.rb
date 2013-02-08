97fff846908b543a3259cac32f7727c5f605901e
Peter Mangiafico <peter@mangiafico.org>
2013-02-07 17:17:27 -0800

    update BCC to CC for contact us form

--- app/mailers/bassi_veratti_mailer.rb
+++ app/mailers/bassi_veratti_mailer.rb
@@ -7,8 +7,8 @@ class BassiVerattiMailer < ActionMailer::Base
     @name=opts[:name]
     @subject=opts[:subject]
     to=BassiVeratti::Application.config.contact_us_recipients[@subject]
-    bcc=BassiVeratti::Application.config.contact_us_bcc_recipients[@subject]    
-    mail(:to=>to,:bcc=>bcc, :subject=>"Contact Message from Bassi Veratti Digital Library - #{@subject}") 
+    cc=BassiVeratti::Application.config.contact_us_cc_recipients[@subject]    
+    mail(:to=>to,:cc=>cc, :subject=>"Contact Message from Bassi Veratti Digital Library - #{@subject}") 
   end
 
   def error_notification(opts={})
