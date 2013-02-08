7ebfd2b189772c322ad02dd3343ec0e02e737e5e
Gary Geisler <ggeisler@gmail.com>
2013-02-08 11:23:33 -0800

    Update collection highlight seed data with real data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,50 @@ end
 CollectionHighlight.find(:all).each {|h| h.destroy}
 CollectionHighlightItem.find(:all).each {|hi| hi.destroy}
 
-c1=CollectionHighlight.create(sort_order:1,name_en:'University Correspondence',name_it:'University Correspondance',description_en:'Items from university correspondance.',description_it:'Items from university correspondance.',image_url:'bv-sample.png')
-c2=CollectionHighlight.create(sort_order:2,name_en:'Official Documents',name_it:'Official Documents',description_en:'Items from official documents.',description_it:'Items from official documents.',image_url:'bv-sample-2.png')
-c3=CollectionHighlight.create(sort_order:3,name_en:'Personal Letters',name_it:'Personal Letters',description_en:'Items from personal letters.',description_it:'Items from personal letters.',image_url:'bv-sample-3.png')
+c1=CollectionHighlight.create(
+  sort_order:1,
+  name_en:'Laura Bassi\'s awards and medals',
+  name_it:'Diplomi di Accademie e medaglia di Laura Bassi',
+  description_en:'',
+  description_it:'',
+  image_url:'bv-sample.png')
+c2=CollectionHighlight.create(
+  sort_order:2,
+  name_en:'Documents dealing with Laura Bassi\'s graduation',
+  name_it:'Documenti relativi alla laurea di Laura Bassi',
+  description_en:'',
+  description_it:'',
+  image_url:'bv-sample-2.png')
+c3=CollectionHighlight.create(
+  sort_order:3,
+  name_en:'Poems in honour of Laura Bassi',
+  name_it:'Poesie in elogio di Laura Bassi',
+  description_en:'',
+  description_it:'',
+  image_url:'bv-sample-3.png')
+c4=CollectionHighlight.create(
+  sort_order:4,
+  name_en:'About Laura Bassi',
+  name_it:'di Laura Bassi',
+  description_en:'',
+  description_it:'',
+  image_url:'bv-sample-3.png')
+c5=CollectionHighlight.create(
+  sort_order:5,
+  name_en:'Documents concerning Veratti family',
+  name_it:'Documenti relativi alla famiglia Veratti',
+  description_en:'',
+  description_it:'',
+  image_url:'bv-sample-3.png')
 
-c1_items=%w{ref18 ref22 ref25 ref46}
-c2_items=%w{ref100 ref106 ref114}
-c3_items=%w{ref333 ref331 ref329 ref327}
+c1_items=%w{ref444 ref436 ref440 ref442 ref446 ref450 ref461 ref464}
+c2_items=%w{ref417 ref596 ref589 ref419 ref423}
+c3_items=%w{ref593 ref586 ref486 ref494 ref510 ref492 ref512 ref547 ref537}
+c4_items=%w{ref599 ref375 ref471 ref613 ref609}
+c5_items=%w{ref272 ref169 ref174 ref216 ref246 ref305 ref329 ref335 ref730 ref736}
 
 add_items(c1_items,c1)
 add_items(c2_items,c2)
 add_items(c3_items,c3)
-
+add_items(c4_items,c4)
+add_items(c5_items,c5)
