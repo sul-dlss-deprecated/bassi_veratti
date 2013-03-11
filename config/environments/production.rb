35a317d2bab3e8c5f73d869d51567f1123a7a6f0
Peter Mangiafico <peter@mangiafico.org>
2013-03-11 14:01:18 -0700

    add google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,7 @@ BassiVeratti::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  GA.tracker = "UA-7219229-18" # bassi
+  
 end
