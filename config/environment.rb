0bdf17bd2fa334083b7a47798ed7d51864a0c4fd
Gary Geisler <ggeisler@gmail.com>
2013-09-27 12:22:11 -0700

    Update location of the embedded image viewer plugin to reflect it now using asset pipeline

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ require File.expand_path('../application', __FILE__)
 BassiVeratti::Application.initialize!
 
 BassiVeratti::Application.config.purl_plugin_server = "prod"
-BassiVeratti::Application.config.purl_plugin_location = "//image-viewer.stanford.edu/javascripts/purl_embed_jquery_plugin.js"
+BassiVeratti::Application.config.purl_plugin_location = "//image-viewer.stanford.edu/assets/purl_embed_jquery_plugin.js"
 BassiVeratti::Application.config.purl = "http://purl.stanford.edu"
