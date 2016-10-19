ee51b69ce59ff67e9ba18dc33d0f26102163180a
Joe Atzberger <ohiocore@gmail.com>
2016-10-19 13:39:04 -0700

    Convert specs to RSpec 3.5.4 syntax with Transpec
    This conversion is done by Transpec 3.2.2 with the following command:
        transpec
    
    * 60 conversions
        from: obj.should
          to: expect(obj).to
    
    * 12 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 8 conversions
        from: == expected
          to: eq(expected)
    
    * 7 conversions
        from: mock('something')
          to: double('something')
    
    * 6 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 4 conversions
        from: =~ /pattern/
          to: match(/pattern/)
    
    * 4 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 3 conversions
        from: be_false
          to: be_falsey
    
    * 3 conversions
        from: be_true
          to: be_truthy
    
    * 2 conversions
        from: obj.stub_chain(:message1, :message2)
          to: allow(obj).to receive_message_chain(:message1, :message2)
    
    * 1 addition
          of: RSpec.configure { |c| c.infer_spec_type_from_file_location! }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe("Home Page", :type => :request, :integration => true) do
   it "should render the home page with some text" do
     visit root_path(:nomap => true)
-    page.should have_content("The online Bassi-Veratti Collection is a multi-year collaboration of the Stanford University Libraries, the Biblioteca Comunale dell'Archiginnasio, Bologna, Italy, and the Istituto per i Beni Artistici, Culturali e Naturali della Regione Emilia-Romagna, to produce a digital version of the archive of the influential woman scientist, Laura Bassi.")
+    expect(page).to have_content("The online Bassi-Veratti Collection is a multi-year collaboration of the Stanford University Libraries, the Biblioteca Comunale dell'Archiginnasio, Bologna, Italy, and the Istituto per i Beni Artistici, Culturali e Naturali della Regione Emilia-Romagna, to produce a digital version of the archive of the influential woman scientist, Laura Bassi.")
   end
 end
