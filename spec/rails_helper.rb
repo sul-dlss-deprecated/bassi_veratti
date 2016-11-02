f2339ca3bdcc2324ce358a63841e9f90f63916ed
Joe Atzberger <ohiocore@gmail.com>
2016-11-01 17:14:56 -0700

    Attempt to clarify backtraces during testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,5 +64,7 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.filter_gems_from_backtrace('rack')
+  config.filter_gems_from_backtrace('sass')
+  config.filter_gems_from_backtrace('sprockets')
 end
