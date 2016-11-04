18f50b596c5aed8d5974bf73fe511e3fb1abd6c8
Joe Atzberger <ohiocore@gmail.com>
2016-11-03 18:52:34 -0700

    Rework facets_helper_spec for new Blacklight::Configuration needs
    Added a second set of tests to check that locale-setting works
    correctly.

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -1,19 +1,17 @@
 module FacetsHelper
   include Blacklight::FacetsHelperBehavior
 
-  # used in the catalog/_facets partial
-  # REMOVE THIS when upgrading to Blacklight > 4.0.1 and use a before_filter in the CatalogController.
-  # before_filter do
-  #   configure_blacklight do |config|
-  #     config.add_facet_field "#{I18n.locale}_document_types_ssim", :label => 'bassi.facet.document_types'
-  #   end
-  # end
-
+  # Display-time filtering: en_document_types_ssim vs. it_document_types_ssim, for example.
+  # Used in the catalog/_facets partial.
+  # @param [Object] display_facet
+  # @note This *could* be replaced by a pre-query filtering of the Blacklight config, such that
+  #   Solr would not return unwanted facets, and display-time filtering would be unnecessary, but
+  #   it is entirely functional as it is, and the direct manipulation of BL config per request is burdensome.
   def should_render_facet?(display_facet)
     facet_patterns = %w(document_types_ssim highlight_ssim)
     super && !(
       facet_patterns.any? { |facet_pattern| display_facet.name.include? facet_pattern } &&
-      !display_facet.name.include?(I18n.locale.to_s + "_")
+      !display_facet.name.start_with?(I18n.locale.to_s + "_")
     )
   end
 end
--- spec/helpers/facets_helper_spec.rb
+++ spec/helpers/facets_helper_spec.rb
@@ -1,11 +1,26 @@
 describe FacetsHelper do
-  describe "Blacklight overrides" do
-    describe "should_render_facet?" do
-      it "should remove all the appropriate facet field names and replace them with the field including the locale" do
-        allow(helper).to receive_message_chain([:blacklight_config, :facet_fields]).and_return("en_document_types_ssim" => double(show: true), "it_document_types_ssim" => double(show: true))
+  describe 'Blacklight overrides' do
+    let(:config) do
+      bc = Blacklight::Configuration.new
+      allow(bc).to receive(:facet_configuration_for_field).with('it_document_types_ssim').and_return double(show: true)
+      allow(bc).to receive(:facet_configuration_for_field).with('en_document_types_ssim').and_return double(show: true)
+      bc
+    end
 
-        expect(helper.should_render_facet?(double(name: "it_document_types_ssim", items: [1, 2, 3]))).to be_falsey
-        expect(helper.should_render_facet?(double(name: "en_document_types_ssim", items: [1, 2, 3]))).to be_truthy
+    before do
+      allow(helper).to receive(:blacklight_config).and_return config
+      allow(helper).to receive(:blacklight_configuration_context).and_return Blacklight::Configuration::Context.new(helper)
+    end
+
+    describe 'should_render_facet?' do
+      it 'should remove all the appropriate facet field names and replace them with the field including the locale' do
+        expect(helper.should_render_facet?(double(name: 'it_document_types_ssim', items: [1, 2, 3]))).to be_falsey
+        expect(helper.should_render_facet?(double(name: 'en_document_types_ssim', items: [1, 2, 3]))).to be_truthy
+      end
+      it 'should remove all the appropriate facet field names and replace them with the field including the locale' do
+        I18n.locale = 'it'
+        expect(helper.should_render_facet?(double(name: 'it_document_types_ssim', items: [1, 2, 3]))).to be_truthy
+        expect(helper.should_render_facet?(double(name: 'en_document_types_ssim', items: [1, 2, 3]))).to be_falsey
       end
     end
   end
