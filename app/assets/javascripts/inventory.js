41a46f1c765794bc93bc372cbaa8c8f9acce2eff
Joe Atzberger <ohiocore@gmail.com>
2016-11-14 15:07:15 -0800

    Bootstrap 2->3 migration: icon to glyphicon

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
-
 	if($("[data-collapse='true']").length > 0) {
 		$("ul", $("[data-collapse='true']")).each(function(){
 			var toggle_text = $(this).children("li[data-behavior='toggle-handler']");
 			toggle_text.each(function(){
 				var icon = $(this).children("i");
 				icon.toggle();
-			  var nested_list = $(this).next("li");	
+			  var nested_list = $(this).next("li");
   			nested_list.hide();
 				$("a, i", $(this)).click(function(){
-					icon.toggleClass("icon-minus-sign");
+                    icon.toggleClass("glyphicon-minus-sign");
 					nested_list.slideToggle();
 					expandedType=nested_list.children().attr('class');
-					if (expandedType == 'folder-items' && icon.is('.icon-minus-sign')) {
+                    if (expandedType == 'folder-items' && icon.is('.glyphicon-minus-sign')) {
 						imageItems=nested_list.find('.item-image-link');
 						loadInventoryImages(imageItems);
  					}
@@ -24,7 +23,7 @@ $(document).ready(function(){
 		// Refresh the scrollspy since we've changed the DOM
 		$('.scrollspy-content').scrollspy("refresh");
 	}
-	
+
 	// When we have an anchor on the inventory page on page load
   if($(".blacklight-inventory").length > 0 && window.location.hash != '') {
 	  var focus = $("[data-reference-id='" + window.location.hash +"']");
@@ -33,10 +32,10 @@ $(document).ready(function(){
 	  nested_list.show();
 		imageItems=nested_list.find('.item-image-link');
 		loadInventoryImages(imageItems);
-	  focus.children("i").toggleClass("icon-minus-sign");
+      focus.children("i").toggleClass("glyphicon-minus-sign");
 	  focus.parents("li:hidden").each(function(){
 		  $(this).show();
-  		$(this).prev("li").children("i").toggleClass("icon-minus-sign");
+        $(this).prev("li").children("i").toggleClass("glyphicon-minus-sign");
 	  });
 	  // Scroll to the item that we're trying to focus on.
 		$(window).scrollTop(focus.offset().top);
@@ -57,5 +56,5 @@ function loadInventoryImages(itemImages) {
 	for (var i = 0 ; i < itemImages.length; i++) {
 		itemImages[i].innerHTML='<a href="' + itemImages[i].attributes['data-image-link'].value + '"><img alt=\'' + itemImages[i].attributes['data-image-title'].value + '\' title=\'' +  itemImages[i].attributes['data-image-title'].value + '\' src="' + itemImages[i].attributes['data-image-url'].value + '"></a>'
 	}
-		
+
 }
