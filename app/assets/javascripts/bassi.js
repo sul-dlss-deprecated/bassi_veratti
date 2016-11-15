82f64ce9d7e03a3e1ef76208bdc4dedab5e2b395
Joe Atzberger <ohiocore@gmail.com>
2016-11-15 11:23:55 -0800

    Whitespace holy war: javascript edition
    Replace tabs.

--- app/assets/javascripts/bassi.js
+++ app/assets/javascripts/bassi.js
@@ -1,25 +1,23 @@
 $(document).ready(function(){
-	
-	// Modal behavior for collection member show page.
-	$("[data-modal-selector]").on('click', function(){
-		$($(this).attr("data-modal-selector")).modal('show');
-	  return false;
-	});
+  // Modal behavior for collection member show page.
+  $("[data-modal-selector]").on('click', function(){
+    $($(this).attr("data-modal-selector")).modal('show');
+    return false;
+  });
 
   $('.scrollspy-content').scrollspy({offset: 30});
 
   $('.overview .nav-pills a:first').tab('show');
 
-	// elements defined with these classes can be hidden by default and then show when the page loads
-	//  useful when you have non javascript friendly DOM elements you need to hide for no JS browsers so you can include a <noscript> tag with
-	//   non JS versions
-	$(".showOnLoad").show();
-	$('.showOnLoad').removeClass('hidden');
+  // elements defined with these classes can be hidden by default and then show when the page loads
+  //  useful when you have non javascript friendly DOM elements you need to hide for no JS browsers so you can include a <noscript> tag with
+  //   non JS versions
+  $(".showOnLoad").show();
+  $('.showOnLoad').removeClass('hidden');
 
   // Responsive width when using the bootstrap affix plugin (which makes width absolute)
   $(function(){
     var sideBarNavWidth=$('#sidebar-nav').width() - parseInt($('.nav-list').css('paddingLeft')) - parseInt($('.nav-list').css('paddingRight'));
     $('.nav-list li').css('width', sideBarNavWidth);
   });
-
 });
--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -1,60 +1,58 @@
 $(document).ready(function(){
-	if($("[data-collapse='true']").length > 0) {
-		$("ul", $("[data-collapse='true']")).each(function(){
-			var toggle_text = $(this).children("li[data-behavior='toggle-handler']");
-			toggle_text.each(function(){
-				var icon = $(this).children("i");
-				icon.toggle();
-			  var nested_list = $(this).next("li");
-  			nested_list.hide();
-				$("a, i", $(this)).click(function(){
-                    icon.toggleClass("glyphicon-minus-sign");
-					nested_list.slideToggle();
-					expandedType=nested_list.children().attr('class');
-                    if (expandedType == 'folder-items' && icon.is('.glyphicon-minus-sign')) {
-						imageItems=nested_list.find('.item-image-link');
-						loadInventoryImages(imageItems);
- 					}
-					// Refresh the scrollspy since we've changed the DOM
-					$('.scrollspy-content').scrollspy("refresh");
-				});
-			});
-		});
-		// Refresh the scrollspy since we've changed the DOM
-		$('.scrollspy-content').scrollspy("refresh");
-	}
+  if($("[data-collapse='true']").length > 0) {
+    $("ul", $("[data-collapse='true']")).each(function(){
+      var toggle_text = $(this).children("li[data-behavior='toggle-handler']");
+      toggle_text.each(function(){
+        var icon = $(this).children("i");
+        icon.toggle();
+        var nested_list = $(this).next("li");
+        nested_list.hide();
+        $("a, i", $(this)).click(function(){
+          icon.toggleClass("glyphicon-minus-sign");
+          nested_list.slideToggle();
+          expandedType=nested_list.children().attr('class');
+          if (expandedType == 'folder-items' && icon.is('.glyphicon-minus-sign')) {
+            imageItems=nested_list.find('.item-image-link');
+            loadInventoryImages(imageItems);
+          }
+          // Refresh the scrollspy since we've changed the DOM
+          $('.scrollspy-content').scrollspy("refresh");
+        });
+      });
+    });
+    // Refresh the scrollspy since we've changed the DOM
+    $('.scrollspy-content').scrollspy("refresh");
+  }
 
-	// When we have an anchor on the inventory page on page load
+  // When we have an anchor on the inventory page on page load
   if($(".blacklight-inventory").length > 0 && window.location.hash != '') {
-	  var focus = $("[data-reference-id='" + window.location.hash +"']");
-	  // Show the next list item and all its hidden list item parents
-		nested_list=focus.next("li");
-	  nested_list.show();
-		imageItems=nested_list.find('.item-image-link');
-		loadInventoryImages(imageItems);
-      focus.children("i").toggleClass("glyphicon-minus-sign");
-	  focus.parents("li:hidden").each(function(){
-		  $(this).show();
-        $(this).prev("li").children("i").toggleClass("glyphicon-minus-sign");
-	  });
-	  // Scroll to the item that we're trying to focus on.
-		$(window).scrollTop(focus.offset().top);
-		// Refresh the scrollspy since we've changed the DOM
-		$('.scrollspy-content').scrollspy("refresh");
+    var focus = $("[data-reference-id='" + window.location.hash +"']");
+    // Show the next list item and all its hidden list item parents
+    nested_list = focus.next("li");
+    nested_list.show();
+    imageItems = nested_list.find('.item-image-link');
+    loadInventoryImages(imageItems);
+    focus.children("i").toggleClass("glyphicon-minus-sign");
+    focus.parents("li:hidden").each(function(){
+      $(this).show();
+      $(this).prev("li").children("i").toggleClass("glyphicon-minus-sign");
+    });
+    // Scroll to the item that we're trying to focus on.
+    $(window).scrollTop(focus.offset().top);
+    // Refresh the scrollspy since we've changed the DOM
+    $('.scrollspy-content').scrollspy("refresh");
   }
 });
 
 function loadAllInventoryImages() {
-	// load all images on the inventory page; not currently used
-	itemImages=$('.item-image-link');
-	loadInventoryImages(itemImages);
+  // load all images on the inventory page; not currently used
+  itemImages = $('.item-image-link');
+  loadInventoryImages(itemImages);
 }
 
 function loadInventoryImages(itemImages) {
-
-	// only load specified images on the inventory page by passing in a dom item
-	for (var i = 0 ; i < itemImages.length; i++) {
-		itemImages[i].innerHTML='<a href="' + itemImages[i].attributes['data-image-link'].value + '"><img alt=\'' + itemImages[i].attributes['data-image-title'].value + '\' title=\'' +  itemImages[i].attributes['data-image-title'].value + '\' src="' + itemImages[i].attributes['data-image-url'].value + '"></a>'
-	}
-
+  // only load specified images on the inventory page by passing in a dom item
+  for (var i = 0 ; i < itemImages.length; i++) {
+    itemImages[i].innerHTML = '<a href="' + itemImages[i].attributes['data-image-link'].value + '"><img alt=\'' + itemImages[i].attributes['data-image-title'].value + '\' title=\'' +  itemImages[i].attributes['data-image-title'].value + '\' src="' + itemImages[i].attributes['data-image-url'].value + '"></a>'
+  }
 }
