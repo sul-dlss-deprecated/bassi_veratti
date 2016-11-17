d3168349d11ff0a5b17fdc28f62a143ea8e9698b
Joe Atzberger <ohiocore@gmail.com>
2016-11-16 16:10:07 -0800

    Reuse returned JQuery object instead of repeating $('selector')

--- app/assets/javascripts/bassi.js
+++ app/assets/javascripts/bassi.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
   // Elements defined with these classes can be hidden by default and then show when the page loads:
   // Useful when you have non-js-friendly DOM elements you need to hide for no-JS browsers
   // so you can include a <noscript> tag with non-JS versions.
-  $(".showOnLoad").show();
-  $('.showOnLoad').removeClass('hidden');
+  $(".showOnLoad").show()
+                  .removeClass('hidden');
 
   // Responsive width when using the bootstrap affix plugin (which makes width absolute)
   $(function(){
