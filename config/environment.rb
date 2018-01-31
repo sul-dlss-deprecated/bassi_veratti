5b9b3d8281defdf0f93bf10d527789b433a4c554
Chris Beer <cabeer@stanford.edu>
2018-01-31 13:47:09 -0800

    Remove unused image viewer javascript

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 BassiVeratti::Application.initialize!
 
-BassiVeratti::Application.config.purl_plugin_server = "prod"
-BassiVeratti::Application.config.purl_plugin_location = "//image-viewer.stanford.edu/assets/purl_embed_jquery_plugin.js"
 BassiVeratti::Application.config.purl = "https://purl.stanford.edu"
